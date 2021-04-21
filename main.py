825aa25fd293367641d34b3b585aac803efaebfb
eetaylor5 <eetaylor@udel.edu>
2021-04-21 14:54:17 -0400

    Added functionality to buttons

--- main.py
+++ main.py
@@ -10,14 +10,48 @@ class AppHomeWindow(GridLayout):
 
        # New functionality
        self.rows = 3
-       self.add_widget(Button(text='SOS text'))
-       self.add_widget(Button(text='911'))
-       self.add_widget(Button(text='Fire Option'))
-       self.add_widget(Button(text='Locked Out'))
-       self.add_widget(Button(text='Power Outage'))
-       self.add_widget(Button(text='Contact Information'))
+       #Define SOS button/functionality
+       sos_btn = Button(text='SOS text')
+       sos_btn.bind(on_press = self.sos_fun)
+       self.add_widget(sos_btn)
+       # Define 911 button/functionality
+       noo_btn = Button(text='911')
+       noo_btn.bind(on_press=self.noo_fun)
+       self.add_widget(noo_btn)
+       # Define fire button/functionality
+       fire_btn = Button(text='Fire Option')
+       fire_btn.bind(on_press=self.fire_fun)
+       self.add_widget(fire_btn)
+       # Define locked out button/functionality
+       locked_out_btn = Button(text='Locked Out')
+       locked_out_btn.bind(on_press=self.locked_out_fun)
+       self.add_widget(locked_out_btn)
+       # Define power out button/functionality
+       power_out_btn = Button(text='Power Outage')
+       power_out_btn.bind(on_press=self.power_out_fun)
+       self.add_widget(power_out_btn)
+       # Define contact info button/functionality
+       contact_info_btn = Button(text='Contact Information')
+       contact_info_btn.bind(on_press=self.contact_info_fun)
+       self.add_widget(contact_info_btn)
 
+   def sos_fun(self, event):
+       print("button pressed, SOS")
 
+   def noo_fun(self, event):
+       print("button pressed, 911")
+
+   def fire_fun(self, event):
+       print("button pressed, fire")
+
+   def locked_out_fun(self, event):
+       print("button pressed, locked out")
+
+   def power_out_fun(self, event):
+       print("button pressed, power out")
+
+   def contact_info_fun(self, event):
+       print("button pressed, contact info")
 
 class KidSafeApp(App):
    def build(self):
