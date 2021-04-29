9c01c6e7fe7b5cfd7eb93f9e915d067f70af9ec2
eetaylor5 <eetaylor@udel.edu>
2021-04-29 02:14:42 -0400

    Switched from using Kivy to KivyMD (for apps)

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.button import Button
 
-class AppHomeWindow(GridLayout):
-   # Override parent method
-   def __init__(self, **kwargs):
-       # Call super to implement original functionality
-       super(AppHomeWindow, self).__init__(**kwargs)
+class KidSafeApp(MDApp):
+   def build(self):
+       self.theme_cls.primary_palette = "Green"
+       self.theme_cls.primary_hue = "A700"
+       self.theme_cls.theme_style = "Dark"
 
-       # New functionality
-       self.cols = 2
+       screen = Screen()
+       btn_grid = MDGridLayout(cols=2)
+       screen.add_widget(btn_grid)
+
        # Define 911 button/functionality
-       noo_btn = Button(text='911')
-       noo_btn.bind(on_press=self.noo_fun)
-       self.add_widget(noo_btn)
-       #Define SOS button/functionality
-       sos_btn = Button(text='SOS text')
-       sos_btn.bind(on_press = self.sos_fun)
-       self.add_widget(sos_btn)
+       noo_btn = Button(text='911', on_press=self.noo_fun)
+       btn_grid.add_widget(noo_btn)
+       # Define SOS button/functionality
+       sos_btn = Button(text='SOS text', on_press=self.sos_fun)
+       btn_grid.add_widget(sos_btn)
        # Define fire button/functionality
-       fire_btn = Button(text='Fire')
-       fire_btn.bind(on_press=self.fire_fun)
-       self.add_widget(fire_btn)
+       fire_btn = Button(text='Fire', on_press=self.fire_fun)
+       btn_grid.add_widget(fire_btn)
        # Define first aid button/functionality
-       first_aid_btn = Button(text='First Aid')
-       first_aid_btn.bind(on_press=self.first_aid_fun)
-       self.add_widget(first_aid_btn)
+       first_aid_btn = Button(text='First Aid', on_press=self.first_aid_fun)
+       btn_grid.add_widget(first_aid_btn)
        # Define stranger danger button/functionality
-       stranger_btn = Button(text='Stranger Danger')
-       stranger_btn.bind(on_press=self.stranger_fun)
-       self.add_widget(stranger_btn)
+       stranger_btn = Button(text='Stranger Danger', on_press=self.stranger_fun)
+       btn_grid.add_widget(stranger_btn)
        # Define severe weather button/functionality
-       weather_btn = Button(text='Severe Weather')
-       weather_btn.bind(on_press=self.weather_fun)
-       self.add_widget(weather_btn)
+       weather_btn = Button(text='Severe Weather', on_press=self.weather_fun)
+       btn_grid.add_widget(weather_btn)
        # Define power out button/functionality
-       power_out_btn = Button(text='Power Outage')
-       power_out_btn.bind(on_press=self.power_out_fun)
-       self.add_widget(power_out_btn)
+       power_out_btn = Button(text='Power Outage', on_press=self.power_out_fun)
+       btn_grid.add_widget(power_out_btn)
        # Define locked out button/functionality
-       locked_out_btn = Button(text='Locked Out')
-       locked_out_btn.bind(on_press=self.locked_out_fun)
-       self.add_widget(locked_out_btn)
+       locked_out_btn = Button(text='Locked Out', on_press=self.locked_out_fun)
+       btn_grid.add_widget(locked_out_btn)
        # Define contact info button/functionality
-       contact_info_btn = Button(text='Contact Information')
-       contact_info_btn.bind(on_press=self.contact_info_fun)
-       self.add_widget(contact_info_btn)
+       contact_info_btn = Button(text='Contact Information', on_press=self.contact_info_fun)
+       btn_grid.add_widget(contact_info_btn)
        # Define parental control button/functionality
-       parent_btn = Button(text='Parental Control')
-       parent_btn.bind(on_press=self.parent_fun)
-       self.add_widget(parent_btn)
+       parent_btn = Button(text='Parental Control', on_press=self.parent_fun)
+       btn_grid.add_widget(parent_btn)
+
+       return screen
+
 
    def noo_fun(self, event):
        print("button pressed, 911")
@@ -80,10 +76,5 @@ class AppHomeWindow(GridLayout):
 
    def parent_fun(self, event):
        print("button pressed, parental control")
-
-
-class KidSafeApp(App):
-   def build(self):
-       return AppHomeWindow()
 
 KidSafeApp().run()
